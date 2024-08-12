a620af50ca42fd5c56be52ddcead1ebb7a0b4e50
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-12 10:31:37 -0300

    test viewsets with django-rest-frameworks

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,11 +1,8 @@
-# bookstore/urls.py
-
+from django.urls import path, re_path, include
 from django.contrib import admin
-from django.urls import path, include
-from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    # path('api/', include('api.urls')), # Comentado
+    re_path(r'bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path(r'bookstore/(?P<version>(v1|v2))/', include('product.urls')),
 ]
