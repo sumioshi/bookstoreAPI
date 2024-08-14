cb5dcac7a80e8e3184336b14f795309428088f9f
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-14 09:50:28 -0300

    import isAuthenticated

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from order.models import Order
 from order.serializers import OrderSerializer
